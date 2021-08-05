6ecb3c2f72d6ba60a1a1098c110a9d650d0f273b
Martin Sisk <Brothermj@hotmail.com>
2021-08-04 19:38:30 -0500

    I added a string food and Console.WriteLine to the Eat Method Of a concatenated string of buddy ate that specific food at the office.

--- Lunch Buddies/LunchBuddy/LunchBuddy.cs
+++ Lunch Buddies/LunchBuddy/LunchBuddy.cs
@@ -13,6 +13,25 @@ namespace Lunch_Buddies.LunchBuddy
         public string LastName { get; set; }
 
 
+        public LunchBuddy(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+
+        public void Eat()
+        {
+            var restaurant = new Restaurant();
+            Console.WriteLine($"{FirstName} {LastName} is at {restaurant.RestaurantName} restaurant.");
+            return restaurant.RestaurantName;
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"{FirstName} {LastName} ate {food} at the office today.");
+        }
+
+
 
     }
 }
