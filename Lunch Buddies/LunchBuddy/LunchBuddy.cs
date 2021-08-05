819907f799edb46e37ef4a46fca612a83a12a1ea
Martin Sisk <Brothermj@hotmail.com>
2021-08-04 19:00:28 -0500

    Folder and File setup with Properties in LunchBuddy and Restaurant.cs Class.

---
+++ Lunch Buddies/LunchBuddy/LunchBuddy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lunch_Buddies.Restaurant;
+
+namespace Lunch_Buddies.LunchBuddy
+{
+    class LunchBuddy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
---
+++ Lunch Buddies/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Lunch_Buddies.LunchBuddy;
+
+namespace Lunch_Buddies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lunch Buddies");
+
+
+
+        }
+    }
+}
